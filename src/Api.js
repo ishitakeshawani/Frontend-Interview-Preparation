aac8d4f13521a9b66f2bbdbd17dd9518291d22d6
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-10-19 13:50:46 +0530

    Implemented API example in react.js

---
+++ src/Api.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import React,{ useState } from 'react'
+
+export default function Api() {
+  const [v,setv] = useState("write here");
+  const url = "https://api.publicapis.org/entries"
+  const fetchApi = () =>{
+    fetch(url).then(res => res.json())
+    .then(
+      p => setv(p.count)
+    )
+    
+  }
+  return (
+    <div className="App">
+      <p>{v}</p>
+      <button onClick={fetchApi}>fetch</button>
+    </div>
+  );
+}
