8e88a9a567f465debfae39f2210b9bf2e7b92b9a
ishitakeshawani <keshwaniishita054@gmail.com>
2021-11-01 15:27:07 +0530

    added  title of 12th question

--- ReactJS_Questions/Q12.js
+++ ReactJS_Questions/Q12.js
@@ -1,3 +1,11 @@
+// Create an Input Element, which shows a display message according to the Input text with the click of a button. If you type - 
+// "Tanvi" - then show, "She is the best CEO ever!"
+// "Tanay" - then show, "He is our FUNNY mentor!"
+// "Swapnil" - then show, "He helps us CONQUER jobs!"
+// "Akanksha" - then show, "She is our CHEERFUL Interviewer!"
+
+
+
 import "./styles.css";
 import React, { useState } from "react";
 
