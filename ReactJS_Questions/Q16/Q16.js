02f2773a1bb445819a33671ed0696ec6b1c69cbb
ishitakeshawani <keshwaniishita054@gmail.com>
2021-11-03 14:28:15 +0530

    added 16th questions

---
+++ ReactJS_Questions/Q16/Q16.js
@@ -0,0 +1,14 @@
+// Create color variables in CSS. Two colors: primary and secondary. Now make your h1 of primary color, h2 of secondary color. Make two buttons, primary and secondary using the same color. Can you also set a font from Google Font?
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>this is h1</h1>
+      <h2>this is h2</h2>
+      <button className="one">1</button>
+      <button className="two">2</button>
+    </div>
+  );
+}
