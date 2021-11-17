0b55b8fbfa605d12b65666c8f77f482e0085ce82
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-11-17 15:26:53 +0530

    Counter using useReducer hook

---
+++ ReactJS_Questions/Q18.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from "react";
+
+const intialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return intialState;
+    default:
+      return state;
+  }
+};
+
+function CounterOne() {
+  const [count, dispatch] = useReducer(reducer, intialState);
+  return (
+    <div>
+      <div>Count - {count}</div>
+      <button onClick={() => dispatch("increment")}>Incremenet</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterOne;
